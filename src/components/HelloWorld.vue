35238f6fa0dfe438b3d92dc096098346f2953360
shubhpy <shubhpycode@gmail.com>
2018-04-04 03:58:07 +0530

    Added autocomplete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,102 @@
 <template>
   <div class="hello" id="app">
-    <h1>{{ msg }}</h1>
-    <!-- <h2>{{ Notifications }}</h2> -->
+    <select v-model="selectPerson">
+      <option value="null" disabled hidden>Select Person</option>
+        <option v-for="p in boardPersons" v-bind:value="p">
+        {{p}}
+        </option>
+    </select>
+
+    <select v-model="selectBoard">
+      <option value="null" disabled hidden>Select Board</option>
+        <option v-for="b in boards" v-bind:value="b">
+        {{b}}
+        </option>
+    </select>
+
+    <h2 v-if="selectPerson"> Board Person : {{ selectPerson }}</h2>
+    <h2 v-if="selectBoard">  Board  : {{ selectBoard }}</h2>
+    
+    <br> </br>
+    Didn't Find your person, Add Now
+    <input type="text" v-model.text="newPerson"> </input>
+    <button v-if="newPerson" @click = "boardPersons.push(newPerson)">
+    Add {{newPerson}}
+    </button>
+
+    <br>
+    Didn't Find the Board, Add Now
+    <input type="text" v-model.text="newBoard"> </input>
+    <button v-if="newBoard" @click = "boards.push(newBoard)">
+    Add {{newBoard}}
+    </button>
+    <!-- <h2>{{ Notifications }}</h2> 
     <ul>
-      <li v-for="notif in Notifications">
-        {{notif.name}} {{notif.message2}} {{notif.message1}}
-      </li>
+      <li v-for="item in items"> {{item}} </li>
+      <li v-for="item in boards"> {{item}} </li>      
     </ul>
+    -->
+
+    <autocomplete :suggestions="suggestions" v-model="selection"></autocomplete>
+    <h2>{{selection}}</h2>
+    
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import Autocomplete from './Autocomplete'
+export default {
+    name: 'app',
+    components: {
+      Autocomplete
+    },
     data() {
       return {
         msg: 'Welcome to Your Vue.js App',
-        Notifications: []
+        selection: '',
+      suggestions: [
+        { city: 'Bangalore', state: 'Karnataka' },
+        { city: 'Chennai', state: 'Tamil Nadu' },
+        { city: 'Delhi', state: 'Delhi' },
+        { city: 'Kolkata', state: 'West Bengal' },
+        { city: 'Mumbai', state: 'Maharashtra' }
+      ],
+        items: [
+          "Stapler",
+          "Stapler pin",
+          "Milk",
+          "Green Tea"
+        ],
+        boardPersons:[
+          "Akash",
+          "Pradeep",
+          "Susheela"
+        ],
+        vendors:[
+          "Horizon",
+          "Aggarwal",
+          "Cafe Coffee"
+        ],
+        boards : [
+          "NABH",
+          "NABET",
+          "WHO"
+        ],
+        selectItem : null,
+        selectBoard : null,
+        selectPerson : null,
+        selectVendor : null,
+        newPerson : null,
+        newBoard : null
       }
-    },
-    created () {
-      fetch('http://localhost:8000/getHomePage')
-      .then(response => response.json())
-      .then(json => {
-        this.Notifications = json.notifications.latest
-      })
     }
+    // created () {
+    //   fetch('http://localhost:8000/getHomePage')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     this.Notifications = json.notifications.latest
+    //   })
+    // }
   }
 </script>
 
